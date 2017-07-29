77c1406e93bc9cba2de190410bf1598fa37cc41c
peerfahad <fahadrashid611@gmail.com>
2017-07-29 11:19:59 +0500

    Way to add by function

---
+++ functions/function_to_add.js
@@ -0,0 +1,12 @@
+var todos = ['item1','item2','item3','item4']
+function addTodo(){
+	todos.push('new todos');
+}
+addTodo()//it will display an updated list
+ //Customizing function by giving paramter
+
+ function addTodo(todos){
+ 	todo.push(todos);
+ }
+ addTodo('new todos')
+ addTodo('new todos 1')
