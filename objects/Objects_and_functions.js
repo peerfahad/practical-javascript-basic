db745b36fc02e165772866f41da3d10d1d043458
peerfahad <fahadrashid611@gmail.com>
2017-08-01 16:34:37 +0500

    objects

---
+++ objects/Objects_and_functions.js
@@ -0,0 +1,7 @@
+var gordon = {
+	name: 'gordon',
+	sayName: function() {
+		console.log(this.name);
+	}
+}
+gordon.sayName(); //it will show  name gordon
---
+++ objects/objects.js
@@ -0,0 +1,9 @@
+var myComputer = {   //myComputer is class and other things are objects
+	operatingSystem: 'mac',
+	screenSize: '15 inch',
+	purchaseYear: '2012',
+};
+
+myComputer //to show all the objects of class
+
+myComputer.operatingSystem //its will result mac
