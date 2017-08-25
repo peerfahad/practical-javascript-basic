0a5277aca23acf298128cd4888cb16d333dbee3f
peerfahad <fahadrashid611@gmail.com>
2017-08-25 22:04:20 +0500

    way to change by class and objects

---
+++ objects/changeTodo_method.js
@@ -0,0 +1,14 @@
+var todosList = {
+  todos:['item1','item2','item3','item4'],
+  display:function(){
+    console.log('Mytodos',this.todos);
+  },
+  addTodo: function(todos){
+    this.todos.push(todos);
+    this.display();
+  },
+  changeTodo: function(position,newValue){
+    this.todos[position] = newValue;
+    this.display();
+  }
+};
